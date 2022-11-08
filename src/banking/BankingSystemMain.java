725bbd84d6d103f3ea85dec36dd4f7cc6f9f5df0
Jaeppy414 <swing0414@gmail.com>
2022-11-08 17:45:55 +0900

    집에서 작업완료

--- src/banking/BankingSystemMain.java
+++ src/banking/BankingSystemMain.java
@@ -22,6 +22,7 @@ public class BankingSystemMain {
 	public static void main(String[] args) {
 		
 		System.out.println("1차 프로젝트(학원)");
+		System.out.println("클론 후 작업01");
 		
 		AccountManager accountmanager = new AccountManager();
 		Scanner scanner = new Scanner(System.in);
